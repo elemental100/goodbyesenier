21c3a97dfa23f16c0779ec7ed88f98df7b3892fa
Pathompong ThonChinda <niceooza@gmail.com>
2021-05-18 22:38:11 +0700

    add showname

--- src/Random.js
+++ src/Random.js
@@ -2,33 +2,56 @@ import React, { useState } from "react";
 import AnimatedNumber from "animated-number-react";
 import axios from 'axios'
 
-const getUsers = async () => {
+
+let id = [""];
+
+const getStuInfo = async () => {
     try {
-        const response = await
-            axios.get("https://yo2h8kjeh9.execute-api.ap-southeast-1.amazonaws.com/productions/getregisterstu")
-        console.log('response: ', response.data.Items);
+        const response = await axios.get("https://yo2h8kjeh9.execute-api.ap-southeast-1.amazonaws.com/productions/getregisterstu")
+        const stuInfo = response.data.Items;
+        id.pop();
+        stuInfo.forEach(it => {
+            id.push(it);
+        });
     } catch (error) {
         console.error(error)
     }
 }
-
-var id = ["6252300306", "6152300307", "6352300308"]
+getStuInfo();
 
 
 export default function Random() {
+
     const [position, setPosition] = useState(0);
-    console.log(id[position])
+    const [name , setName] = useState("");
+    console.log("id",id)
     return (
-        <div>
+        <div style={{ textAlign: "center" }}>
             <AnimatedNumber
-                value={id[position]}
-                duration={300}
+                value={id[position].stdId}
+                duration={1000}
                 formatValue={(value) => value.toFixed(0)}
             />
             <div>
-                <button onClick={() => setPosition([Math.floor(Math.random() * id.length)])}>
-                    Retry
+                <button onClick={() => {
+                    setPosition([Math.floor(Math.random() * id.length)])
+                    setName("");
+                }
+                }>
+                    Random
                 </button>
+            </div>
+            <div>
+            <button onClick={() => {
+                    setName(`${id[position].fName} ${id[position].lName}`);
+                    // id.splice([position],1);
+                }
+                }>
+                    Showname
+                </button>
+            </div>
+            <div>
+                {name}
             </div>
         </div>
     );
