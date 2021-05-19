3ffa374fea47e79255994fcdc12a6a7c96bff3a0
NNICEEt <nniceet.teerapat@github.com>
2021-05-19 18:22:29 +0700

    เพิ่ม MainPage & JSON File

---
+++ src/components/ETByenior/Main.js
@@ -0,0 +1,26 @@
+import Majors from './majors.json';
+import { Redirect, useLocation } from 'react-router-dom';
+
+const majorsData = Majors; 
+
+
+const Main = (props) => {
+
+    const location = useLocation();
+    const stdName = location.state.name;
+    const stdID = location.state.stdID;
+    const major = majorsData.filter((major) => major.mjId === stdID.substring(3, 6))
+
+    if(!props.authorized){
+        return <Redirect to='/'/>
+    }
+
+
+    return(
+        <div>
+            {stdName} รหัสนักศึกษา {stdID} สาขา {major[0].mjName}
+        </div>
+    )
+}
+
+export default Main;
